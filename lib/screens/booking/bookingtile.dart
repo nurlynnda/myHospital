892fc5525e666fd8e41cea82a17dd54dd0c7b648
nurlynnda <nurlynnda@graduate.utm.my>
2022-02-03 17:35:11 +0800

    appoinement_backup.json

--- lib/screens/booking/bookingtile.dart
+++ lib/screens/booking/bookingtile.dart
@@ -16,34 +16,39 @@ class BookingTile extends StatelessWidget {
         builder: (_, vm, booking, ___) {
           print('Build ListTile ');
           return Center(
-            child: Container(
-                margin: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                    color: Colors.cyan[800],
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(1),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3),
-                      )
-                    ]),
-                width: double.infinity,
-                height: 100,
-                child: Column(
-                  children: [
-                    Text(
-                      "Doctor: ${booking.doctor} \nID: ${booking.id} \nSpecialist: ${booking.department} \nTime available: ${booking.time}",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
+              child: Card(
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              child: Container(
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                      color: Colors.cyan[800],
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(1),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        )
+                      ]),
+                  width: double.infinity,
+                  height: 100,
+                  child: Column(
+                    children: [
+                      Text(
+                        "Doctor: ${booking.doctor} \nID: ${booking.id} \nSpecialist: ${booking.department} \nTime available: ${booking.time}",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
                       ),
-                    ),
-                  ],
-                )),
-          );
+                    ],
+                  )),
+              onTap: () => vm.getbooking(index),
+            ),
+          ));
         });
   }
 }
